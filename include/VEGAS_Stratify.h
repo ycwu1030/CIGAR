6af236c92fbc3fbd418c149315eebbb9753d3110
Yongcheng Wu <yc_wu1030@126.com>
2020-10-18 17:39:42 -0400

    Update VEGAS Stratify class and Integrator
    Update VEGAS Integrator to accomondate the Stratification

--- include/VEGAS_Stratify.h
+++ include/VEGAS_Stratify.h
@@ -12,26 +12,28 @@ private:
     double V_cubic;
     std::vector<double> JF2; // size = (N_STRAT)^(N_DIM)
     std::vector<double> JF; // size = (N_STRAT)^(N_DIM)
-    std::vector<int> nh; // size = (N_STRAT)^(N_DIM)
+    std::vector<double> Counts; // size = (N_STRAT)^(N_DIM)
     std::vector<double> dh; // size = (N_STRAT)^(N_DIM)
+    int N_EVALUATES_TRAINED; // The evaluates number used to train the stratification
     int N_EVALUATES_EXPECTED;
-    int N_EVALUATES_ACTUALLY;
     int N_HYPERCUBICS;
 
     void Reset_Storage();
     std::vector<int> Get_Indices(int index);
 
 public:
-    VEGAS_Stratify(){N_DIM = 1; N_EVALUATES_EXPECTED = 10000; beta = 0.75;};
+    VEGAS_Stratify(){N_DIM = 1; N_EVALUATES_TRAINED = 10000; beta = 0.75;};
     ~VEGAS_Stratify(){};
 
-    void Set_Stratification_System(int N_DIM, int NEVAL);
+    void Set_Stratification_System(int N_DIM, int NEVAL_TRAIN);
     void Set_Dimension(int N_DIM);
-    void Set_NEVAL(int NEVAL);
-    void Accumulate_Weights(int index, double weight);
-    void Update_NH();
+    void Set_NEVAL(int NEVAL_EXP);
+    void Accumulate_Weight(int index, double weight);
+    void Update_DH();
     std::vector<double> Get_Y(int index, std::vector<double> random_uni);
-    int Get_NH(int index){}; // Get the expected number of events in each hypercubic.
+    int Get_NHYPERCUBICS(){return N_HYPERCUBICS;};
+    int Get_NH(int index); // Get the expected number of events in each hypercubic.
+    double Get_V_Cubic(){return V_cubic;}
 };
 
 
--- src/VEGAS_Stratify.cpp
+++ src/VEGAS_Stratify.cpp
@@ -1,4 +1,5 @@
 #include "VEGAS_Stratify.h"
+#include <cmath>
 
 using namespace std;
 
@@ -6,30 +7,27 @@ void VEGAS_Stratify::Set_Dimension(int ndim)
 {
     N_DIM = ndim;
 }
-void VEGAS_Stratify::Set_NEVAL(int NEVAL)
+void VEGAS_Stratify::Set_NEVAL(int NEVAL_EXP)
 {
-    N_EVALUATES_EXPECTED = NEVAL;
+    N_EVALUATES_EXPECTED = NEVAL_EXP;
 }
-void VEGAS_Stratify::Set_Stratification_System(int ndim, int NEVAL)
+void VEGAS_Stratify::Set_Stratification_System(int ndim, int NEVAL_TRAIN)
 {
     N_DIM = ndim;
-    N_EVALUATES_EXPECTED = NEVAL;
+    N_EVALUATES_TRAINED = NEVAL_TRAIN;
     Reset_Storage();
 }
 void VEGAS_Stratify::Reset_Storage()
 {
-    N_STRAT = floor(N_EVALUATES_EXPECTED/4.0,1.0/N_DIM);
+    N_STRAT = floor(pow(N_EVALUATES_TRAINED/4.0,1.0/N_DIM));
     
     N_HYPERCUBICS = pow(N_STRAT,N_DIM);
 
     V_cubic = pow(1.0/N_STRAT, N_DIM);
     JF2 = vector<double>(N_HYPERCUBICS,0);
     JF  = vector<double>(N_HYPERCUBICS,0);
-    nh  = vector<int>(N_HYPERCUBICS,N_EVALUATES_EXPACTED/N_HYPERCUBICS);
-    dh  = vector<double>(N_HYPERCUBICS,0);
-    
-    N_EVALUATES_ACTUALLY = 0;
-
+    Counts = vector<double>(N_HYPERCUBICS,0);
+    dh  = vector<double>(N_HYPERCUBICS,1.0/N_HYPERCUBICS);
 }
 vector<int> VEGAS_Stratify::Get_Indices(int index)
 {
@@ -57,26 +55,30 @@ vector<double> VEGAS_Stratify::Get_Y(int index, vector<double> random_uni)
     }
     return res;
 }
-void VEGAS_Stratify::Accumulate_Weights(int index, double weight)
+void VEGAS_Stratify::Accumulate_Weight(int index, double weight)
 {
     // This weight is J*f;
     JF2[index] += weight*weight;
     JF[index] += weight;
+    Counts[index] += 1;
 }
-void VEGAS_Stratify::Update_NH()
+void VEGAS_Stratify::Update_DH()
 {
     double d_sum = 0;
     double d_tmp;
     for (int i = 0; i < N_HYPERCUBICS; i++)
     {
-        d_tmp = V_cubic*V_cubic/nh[i]*JF2[i] - pow(V_cubic/nh[i]*JF[i],2);
+        d_tmp = V_cubic*V_cubic/Counts[i]*JF2[i] - pow(V_cubic/Counts[i]*JF[i],2);
         dh[i] = pow(d_tmp,beta);
         d_sum += dh[i];
     }
-    int nh_tmp;
     for (int i = 0; i < N_HYPERCUBICS; i++)
     {
-        nh_tmp = N_EVALUATES_EXPECTED * dh[i]/d_sum;
-        nh[i] = nh_tmp >= 2?nh_tmp:2;
+        dh[i] = dh[i]/d_sum;
     }
+}
+int VEGAS_Stratify::Get_NH(int index)
+{
+    int nh = dh[index]*N_EVALUATES_EXPECTED;
+    return nh<2?2:nh;
 }
